f42c167e0428b48b93f519e5ed15151c7c11f84f
Amanda Mates <mates.j.amanda@gmail.com>
2018-01-13 13:51:08 -0500

    second

--- key.js
+++ key.js
@@ -1,14 +1,13 @@
-console.log('this is loaded'); 
+console.log('this is loaded');
 
-exports.twitter = { 
-	consumer_key: process.env.Q8CXtnr4wBRdmiFOxQsdpYtrV;  
-	consumer_secret: process.env.lkOaWcQuaUYawDgAaZ2rNFFjEzNu954TFWPx81PJquScUGbEPW;
-	access_token_key: process.env.951183635436179461-CB0FNsEeP0nWWcGIeSdqUAa9uTb6duF; 
-	access_token_secret: process.env.bPvYqGQkRfG2ChbX61uBxyBAO6REHdjh82i8zz8OcSvlF; 
+exports.twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-	id: process.env.93a072918fc4c219a7e302f9880e5b9; 
-	secret: process.env.b1e2191ee570456e8276d65a12808ce6;
-}; 
-
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
--- liri.js
+++ liri.js
@@ -5,29 +5,45 @@ var Twitter = require("twitter");
 var spotifyAPI = require("node-spotify-api");
 var request = require("request"); 
 
+
+
+var command = process.argv[2];
+var track = process.argv[3];
+
+switch (command) {
+	case "my-tweets": 
+		myTweets();
+};
+
+
+
 function myTweets() {
-	var client = new twitter(key)
+	var client = new Twitter(key);
 	var parameters = {
 		screen_name: 'slowtocode', 
 		count: 20
-	}
+	};
 client.get('statuses/user_timeline', parameters, function(error, tweets, response){
 	if (error){
 		console.log(error);
 	}
 	else {
-		console.log(tweets); 
-		}
-	})
+		console.log("Amanda's tweets");
+		tweets.forEach(function(userTweets){
+			console.log("Date: " + userTweets.created_at);
+			console.log(userTweets.text);
+		}); 
+		};
+	});
 }; 
 
 function spotifyThisSong(track) {
 	if (track === undefined){
-		console.log("The Sign by Ace of Base");
+		track = "The Sign by Ace of Base";
 	}
 var spotify = new spotifyAPI({
-	id: 93a072918fc4c219a7e302f9880e5b9
-	secret: b1e2191ee570456e8276d65a12808ce6
+	id: "493a072918fc4c219a7e302f9880e5b9",
+	secret: "b1e2191ee570456e8276d65a12808ce6"
 });
 spotify.serch({type: 'track', query: track}, function(error, data){
 	if (error) {
